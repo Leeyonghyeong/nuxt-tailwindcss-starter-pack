c20073de0b4616a672db1788565685d1afe49fbc
tont1990 <tont1990@gmail.com>
2025-05-12 18:23:24 +0900

    Main Page 수정

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,9 @@
 <template>
   <div class="flex flex-col gap-10 justify-center items-center w-full h-screen">
-    <div class="text-5xl">{{ store.storeVar }}</div>
-    <div class="cursor-pointer" @click="$router.push('/test')">페이지 이동</div>
+    <div class="text-5xl">Hello! Nuxt 3 + TailwindCSS 4 Starter Pack</div>
   </div>
 </template>
 
-<script lang="ts" setup>
-import { useStore } from '~/stores/useStore'
-
-const store = useStore()
-</script>
+<script lang="ts" setup></script>
 
 <style lang="scss" scoped></style>
